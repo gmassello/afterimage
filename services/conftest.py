ce7764f72fc9019cc66f4b4dec2ea899ea6e232e
gmassello <gmassello@gmail.com>
2026-08-26 22:20:29 -0300

    Refactor memory access to use a shared runs module.

--- services/conftest.py
+++ services/conftest.py
@@ -6,6 +6,10 @@ os.environ.setdefault("AWS_ACCESS_KEY_ID", "test")
 os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "test")
 os.environ.setdefault("AWS_DEFAULT_REGION", "us-east-1")
 
+localstack = pytest.mark.skipif(
+    "AWS_ENDPOINT_URL" not in os.environ, reason="requires LocalStack"
+)
+
 
 @pytest.fixture(scope="session", autouse=True)
 def memory_backends():
